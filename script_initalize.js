6309758d30f67feee1f6e0ffe92712bfadcf28af
hmm yes <143506127+porystyrene@users.noreply.github.com>
2025-02-17 15:14:57 +0800

    added shit

---
+++ script_initalize.js
@@ -0,0 +1,2 @@
+var _camx = 0;
+var _camy = 0;
--- script_resize.js
+++ script_resize.js
@@ -1,18 +1,6 @@
 //NEED TO TEST
 
 function resize() {
-if (typeof resize_width !== "number") {
-resize_width = 0;
-resize_height = 0;
-};
-
-var resize_actualWidth = window.innerWidth;
-var resize_actualHeight = window.innerHeight;
-
-if (!(resize_width == resize_actualWidth && resize_height == resize_actualHeight)) {
-document.getElementById("_body").style = "width: " + resize_actualWidth + "px; height: " + resize_actualHeight + "px; background-color: black;";
-document.getElementById("_canvas").style = "width: " + resize_actualWidth + "px; height: " + resize_actualHeight + "px;";
-resize_width = resize_actualWidth;
-resize_height = resize_actualHeight;
-};
+    document.getElementById("_body").style = "width: " + window.innerWidth + "px; height: " + window.innerHeight + "px;";
+    document.getElementById("_canvas").style = "width: " + window.innerWidth + "px; height: " + window.innerHeight + "px;";
 }
