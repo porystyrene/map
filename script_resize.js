0211311ce1cc23485f6e048476937b87ef4d0f80
hmm yes <143506127+porystyrene@users.noreply.github.com>
2025-02-17 14:14:22 +0800

    edited resize(), still need to test

--- script_resize.js
+++ script_resize.js
@@ -1,36 +1,18 @@
 //NEED TO TEST
 
 function resize() {
-if (typeof Function_SetCanvasAndBodySize_Width !== "number") {
-Function_SetCanvasAndBodySize_Width = 0;
-Function_SetCanvasAndBodySize_Height = 0;
+if (typeof resize_width !== "number") {
+resize_width = 0;
+resize_height = 0;
 };
 
-var Game_WindowWidth = window.innerWidth;
-var Game_WindowHeight = window.innerHeight;
+var resize_actualWidth = window.innerWidth;
+var resize_actualHeight = window.innerHeight;
 
-if (!(Function_SetCanvasAndBodySize_Width == Game_WindowWidth && Function_SetCanvasAndBodySize_Height == Game_WindowHeight)) {
-document.getElementById("__body").style = "width: " + Game_WindowWidth + "px; height: " + Game_WindowHeight + "px; background-color: black;";
-document.getElementById("__canvas").style = "width: " + Game_WindowWidth + "px; height: " + Game_WindowHeight + "px;";
-Function_SetCanvasAndBodySize_Width = Game_WindowWidth;
-Function_SetCanvasAndBodySize_Height = Game_WindowHeight;
-
-const Game_GoalWidth = 1000;
-const Game_GoalHeight = 1000;
-
-if ((Game_WindowWidth / Game_WindowHeight) == (Game_GoalWidth / Game_GoalHeight)) {
-//same ratio
-document.getElementById("__canvas").width = Game_GoalWidth;
-document.getElementById("__canvas").height = Game_GoalHeight;
-} else if ((Game_WindowWidth / Game_WindowHeight) > (Game_GoalWidth/Game_GoalHeight)) {
-//height is smaller
-document.getElementById("__canvas").width = Game_GoalWidth;
-document.getElementById("__canvas").height = (Game_GoalWidth / Game_WindowWidth) * Game_WindowHeight;
-} else {
-//width is smaller
-document.getElementById("__canvas").height = Game_GoalHeight;
-document.getElementById("__canvas").width = (Game_GoalHeight / Game_WindowHeight) * Game_WindowWidth;
-};
-
+if (!(resize_width == resize_actualWidth && resize_height == resize_actualHeight)) {
+document.getElementById("__body").style = "width: " + resize_actualWidth + "px; height: " + resize_actualHeight + "px; background-color: black;";
+document.getElementById("__canvas").style = "width: " + resize_actualWidth + "px; height: " + resize_actualHeight + "px;";
+resize_width = resize_actualWidth;
+resize_height = resize_actualHeight;
 };
 }
