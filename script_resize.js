f0f1f4f3f955a4f091601c7fe99704905181a0f4
hmm yes <143506127+porystyrene@users.noreply.github.com>
2025-02-17 14:20:57 +0800

    updated ids

--- script_resize.js
+++ script_resize.js
@@ -10,8 +10,8 @@ var resize_actualWidth = window.innerWidth;
 var resize_actualHeight = window.innerHeight;
 
 if (!(resize_width == resize_actualWidth && resize_height == resize_actualHeight)) {
-document.getElementById("__body").style = "width: " + resize_actualWidth + "px; height: " + resize_actualHeight + "px; background-color: black;";
-document.getElementById("__canvas").style = "width: " + resize_actualWidth + "px; height: " + resize_actualHeight + "px;";
+document.getElementById("_body").style = "width: " + resize_actualWidth + "px; height: " + resize_actualHeight + "px; background-color: black;";
+document.getElementById("_canvas").style = "width: " + resize_actualWidth + "px; height: " + resize_actualHeight + "px;";
 resize_width = resize_actualWidth;
 resize_height = resize_actualHeight;
 };
