d0b15c5d975d9e87e378bfe6e94ee0c0d1ca1dad
hmm yes <143506127+porystyrene@users.noreply.github.com>
2025-02-17 15:16:23 +0800

    shit

--- script_initalize.js
+++ script_initalize.js
@@ -1,2 +1,3 @@
 var _camx = 0;
 var _camy = 0;
+var _camscale = 1;
--- script_render.js
+++ script_render.js
@@ -1,7 +1,7 @@
 function render() {
-const canvas = document.getElementById("_canvas");
-const ctx = canvas.getContext("2d");
-const img = new Image();
-img.src = "image.png";
-ctx.drawImage(img, _camx - canvas.width/2, _camy + canvas.height/2);
+    const canvas = document.getElementById("_canvas");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.src = "image.png";
+    ctx.drawImage(img, _camx - canvas.width/2, _camy + canvas.height/2);
 }
